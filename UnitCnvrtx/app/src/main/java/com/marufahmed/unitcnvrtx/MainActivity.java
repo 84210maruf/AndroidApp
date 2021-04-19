1b4eaab106b800be66033114f5da09f2caa5dddc
maruf <maruf84210cse@gmail.com>
2021-04-19 23:58:42 +0600

    add some style in unitConvert app

--- UnitCnvrtx/app/src/main/java/com/marufahmed/unitcnvrtx/MainActivity.java
+++ UnitCnvrtx/app/src/main/java/com/marufahmed/unitcnvrtx/MainActivity.java
@@ -2,6 +2,7 @@ package com.marufahmed.unitcnvrtx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,8 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextView textView;
     private EditText editText;
-
-
+    private TextView textView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +26,10 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
         editText = findViewById(R.id.editText);
+        textView4 = findViewById(R.id.textView4);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            String msg = "Hi It's me!";
+//  FIRST WAY!(01)
+       /* button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -39,12 +40,28 @@ public class MainActivity extends AppCompatActivity {
                 // Calculate kg to pounds
                 double pound = 2.250 * kg;
                 //Show the text in app
-                textView.setText("The corresponding value in POUND is "+pound);
-                Toast.makeText(MainActivity.this, this.msg, Toast.LENGTH_SHORT).show();
+                editText.setText("     "+kg+" KG = "+pound+" Pound");
+                // Toast.makeText(MainActivity.this, this.msg, Toast.LENGTH_SHORT).show();
+                }
+            });
+         }*/
 
-            }
-        });
+
+
     }
+
+
+
+// OTHER WAY(02)
+        public void calculate(View view){
+
+            textView4 = findViewById(R.id.textView4);
+
+            String s = editText.getText().toString();
+            int kg = Integer.parseInt(s);
+            double pound = 2.250 * kg;
+            textView4.setText(kg+" KG = "+pound+" Pound");
+        }
 
 
 
