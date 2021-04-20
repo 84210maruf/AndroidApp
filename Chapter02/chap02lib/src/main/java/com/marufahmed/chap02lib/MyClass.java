544199c78a7aba5a4538d4039c0f50e0932c6311
maruf <maruf84210cse@gmail.com>
2021-04-20 21:03:14 +0600

    just chap02 add

---
+++ Chapter02/chap02lib/src/main/java/com/marufahmed/chap02lib/MyClass.java
@@ -0,0 +1,8 @@
+package com.marufahmed.chap02lib;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hallow World(Lib)");
+        System.out.println("MARUF AHMED");
+    }
+}
